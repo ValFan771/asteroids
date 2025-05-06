767735b825a2ec85343d4f04d71c100b2a1808a7
ValFan771 <svobodam.2002@pm.me>
2025-05-06 14:00:10 +0200

    Added rendering of the player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 
+from player import Player
 from constants import *
 
 
@@ -8,14 +9,16 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0 # Delta time
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: #making the close button on window work
                 return
         screen.fill((0,0,0)) #filling frame with full black
-        pygame.display.flip() #rendering the frame
+        player.draw(screen)
         time = clock.tick(60) #waiting for 1/60th of a second
         dt = time / 1000.0 # calculating delta time in seconds
+        pygame.display.flip() #rendering the frame
 
 if __name__ == "__main__":
     main()
