8ee2c0bddb52d29a98d9a62c57c23affd68c9f04
ValFan771 <svobodam.2002@pm.me>
2025-05-11 14:55:57 +0200

    Fixed spamming of shots

--- player.py
+++ player.py
@@ -1,13 +1,14 @@
 import pygame
 
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN
 from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.cooldown = 0
 
     # Handles math for calculating the shape of the player
     def triangle(self):
@@ -29,6 +30,8 @@ class Player(CircleShape):
     # Handles updating the player from main.py
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        if self.cooldown > 0:
+            self.cooldown -= dt
 
         if keys[pygame.K_a]:
             self.rotate(dt * -1)
@@ -39,7 +42,9 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(dt * -1)
         if keys[pygame.K_SPACE]:
-            self.shoot()
+            if self.cooldown <= 0:
+                self.shoot()
+                self.cooldown = PLAYER_SHOOT_COOLDOWN
 
     # Handles movement of the player
     def move(self, dt):
